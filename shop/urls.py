34fb5a56f99061a33eb674cb5e519e3efd4dcfb8
stagepalete2 <edige.rakhatuly@gmail.com>
2024-08-24 09:22:46 +0500

    Add: Catalog page

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, catalog
 
 urlpatterns = [
     path('', home, name='home'),
+    path('catalog/', catalog, name='catalog')
 ]
