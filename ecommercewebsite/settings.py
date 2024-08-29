19d321c848db98a096fa90bf5690f4777b41e953
stagepalete2 <edige.rakhatuly@gmail.com>
2024-08-29 15:18:34 +0500

    Fix: Installed new django bootstrap lib because of conflict

--- ecommercewebsite/settings.py
+++ ecommercewebsite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django_bootstrap5',
     
     'users',
     'shop',
@@ -143,3 +143,4 @@ MEDIA_ROOT = os.getenv('MEDIA_ROOT')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
