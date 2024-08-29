b740cc2e3e2914883caaf5f702c44a120b86a3ba
stagepalete2 <edige.rakhatuly@gmail.com>
2024-08-29 15:22:13 +0500

    Add: Forms

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from .forms import SignInForm
+
 # Create your views here.
+def profile(request):
+    return render(request, template_name='profile.html')
+
+def edit_profile(request):
+    return render(request, template_name='edit.html')
+
+def signin(request):
+    form = SignInForm
+    context = {
+        'form' : form,
+    }
+    return render(request, template_name='auth/signin.html', context=context)
+
+
+def signup(request):
+    return render(request, template_name='auth/signup.html')
